7bd21724b567a60426d0ad9a08d1e7320c653e4b
Manoel Serafim <m4n0e1.s3r4f1m@gmail.com>
2023-11-17 14:34:10 -0300

    [+] test files [structure [@] main

---
+++ main.c
@@ -0,0 +1,48 @@
+#include "include/lexer.h"
+
+
+/*  [main function - returns status]  */
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        //NOT ENOUGH ARGS
+        warn("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE * stream = fopen( argv[1] , "r" );
+    if (stream == NULL) {
+        //FOPEN ERROR
+        perror(RED"fopen() error"YELLOW);
+        return 1;
+    }
+
+    Buffer buffer;
+    allocateBuffer(&buffer, stream);
+
+
+    TokenRecord* token = malloc(sizeof(TokenRecord)); 
+
+
+    int count;
+    do{
+        getNextToken(&buffer, stream, token);
+        if(token->type == EOF){
+            break;
+        }
+        if(token->type == INVALID){
+            return EXIT_FAILURE;
+        }
+        //parser here
+    }while(token->type != EOF);
+
+    
+    //cleanup functions
+    free(token);
+    deallocateBuffer(&buffer);
+    fclose(stream);
+
+
+
+    return EXIT_SUCCESS;
+}
