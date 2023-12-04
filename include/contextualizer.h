579c506dc1348e25154a2a4ca365c178e3ce5d0a
Manoel Serafim <m4n0e1.s3r4f1m@gmail.com>
2023-12-04 20:02:10 -0300

    contextualizer start

---
+++ include/contextualizer.h
@@ -0,0 +1,8 @@
+#ifndef __CONTEXTUALIZER_H_
+#define __CONTEXTUALIZER_H_
+
+/*--[Semantic Analyzer]--*/
+/*[create symbol table and check for correct var typing]*/
+void contextualize(syntax_t_node*);
+
+#endif
---
+++ src/contextualizer.c
@@ -0,0 +1,11 @@
+#include "contextualizer.h"
+#include "parser.h"
+
+/*--[Syntax Tree Traversal]--*/
+/*[path can be from front to back(symbol table) and back to front(type check)]*/
+
+/*--[Semantics Analyser]--*/
+/*Type check && Symbol Table*/
+void contextualize(syntax_t_node * root){
+
+}
