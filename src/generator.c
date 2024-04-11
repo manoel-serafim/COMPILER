5c9f1f0460d275afbe09db252ed3ebbc46f0fdb7
Manoel Serafim <manoel.serafim@proton.me>
2024-04-10 21:19:01 -0300

    <fact> :: adding structure for all statement kinds

--- src/generator.c
+++ src/generator.c
@@ -17,11 +17,16 @@ static void generate_statement( syntax_t_node* branch )
 {
     switch(branch->has.exp.kind)
     {
-        case OP_EK:
-        case ID_EK:
-        case NUM_EK:
-        case TYPE_EK:
-        case VECT_ID_EK:
+        case IF_SK:
+        case WHILE_SK:
+        case RETURN_SK:
+        case ASSIGN_SK:
+        case VAR_SK:
+        case VECT_SK:
+        case FUNCT_SK:
+        case CALL_SK:
+        case PARAM_SK:
+        case VECT_PARAM_SK:
     }
 
 }
@@ -38,6 +43,7 @@ void generate(syntax_t_node* syntax_root){
                 generate_expression(syntax_root)
                 break;
             case STMT_T:
+                generate_expression(syntax_root)
                 break;
         }
 
